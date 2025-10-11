edfc5d83c4f68ec570434ba019fc4ccfcf59a9f1
eunkyung <yhr05008@naver.com>
2025-10-12 02:33:33 +0900

    test: UserCreateControllerTest  테스트 코드 추가

--- src/test/java/com/example/demo/controller/UserCreateControllerTest.java
+++ src/test/java/com/example/demo/controller/UserCreateControllerTest.java
@@ -1,13 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.UserCreateDto;
+import com.example.demo.model.dto.UserUpdateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -19,6 +32,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureTestDatabase
 // 테스트용 인메모리 DB(H2 등) 로 자동 설정
 // 실제 DB에 영향없이 테스트 데이터 관리 가능
+@SqlGroup({
+        // 테스트 종류 후 데이터 정리
+        @Sql(value = "/sql/delete-all-data.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
 class UserCreateControllerTest {
 
     @Autowired
@@ -26,12 +43,36 @@ class UserCreateControllerTest {
     // HTTP 요청/응답을 시뮬레이션할 객체
     // 컨트롤러 단위 테스트 또는 통합 테스트 시 사용
 
+    @MockBean
+    private JavaMailSender javaMailSender;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+
 
     @Test
-    void 헬스_체크_응답이_200으로_내려온다() throws Exception {
-        // /health_check.html로 GET 요청을 보내고
-        // HTTP 상태 코드가 200(OK)인지 검증
-        mockMvc.perform(get("/health_check.html")).andExpect(status().isOk());
+    void 사용자는_회원가입을_할_수_있고_회원가입된_사용자는_PENDING_상태이다() throws Exception {
+        // given
+        UserCreateDto userCreateDto = UserCreateDto.builder()
+                .email("yhr05008@naver.com")
+                .address("seoul-2")
+                .nickname("ek-2")
+                .build();
+
+        // when
+        BDDMockito.doNothing().when(javaMailSender).send(any(SimpleMailMessage.class));
+
+
+        // then
+        mockMvc.perform(post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.nickname").value("ek-2"))
+                .andExpect(jsonPath("$.email").value("yhr05008@naver.com"))
+                .andExpect(jsonPath("$.status").value(UserStatus.PENDING.toString()));
+
     }
 
 
