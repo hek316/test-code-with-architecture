ad325a7031767dfc1be4c5d840f7ad10644dda30
eunkyung <yhr05008@naver.com>
2026-03-05 23:26:00 +0900

    코드 리네이밍 및 불필요한 코드 제거
     메서드명 변경 createUser ->  create
     UserService자체가 user에 대한 책임을 가지로 있기 때문에 create만 해도 user를 생성한다는 의미를 가짐
    
     getByIdOrElseThrow -> getById
     get은 애초에 데이터가 없으면 오류를 던진다는 의미가 내포되어있기때문에 이름 단순화

---
+++ src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -0,0 +1,85 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.UserStatus;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest(showSql = true)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void UserRepository_가_제대로_연결되었다() {
+        // given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setNickname("test");
+
+        // when
+        UserEntity findUser = userRepository.save(userEntity);
+
+        // then
+        assertThat(findUser.getNickname()).isEqualTo("test");
+    }
+
+    @Test
+    void  findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setNickname("test");
+        userEntity.setStatus(UserStatus.PENDING);
+        UserEntity findUser = userRepository.save(userEntity);
+
+        // when
+        Optional<UserEntity> byIdAndStatus = userRepository.findByIdAndStatus(findUser.getId(), UserStatus.PENDING);
+
+        // then
+        Assertions.assertThat(byIdAndStatus.isPresent()).isTrue();
+    }
+
+    @Test
+    void  findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
+        // given
+        // when
+        Optional<UserEntity> byIdAndStatus = userRepository.findByIdAndStatus(2L, UserStatus.PENDING);
+
+        // then
+        Assertions.assertThat(byIdAndStatus.isEmpty()).isTrue();
+    }
+
+    @Test
+    void  ffindByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
+        // given
+        UserEntity userEntity = new UserEntity();
+        userEntity.setNickname("test");
+        userEntity.setStatus(UserStatus.PENDING);
+        userEntity.setEmail("test");
+        UserEntity findUser = userRepository.save(userEntity);
+
+        // when
+        Optional<UserEntity> byIdAndStatus = userRepository.findByEmailAndStatus(findUser.getEmail(), UserStatus.PENDING);
+
+        // then
+        Assertions.assertThat(byIdAndStatus.isPresent()).isTrue();
+    }
+
+    @Test
+    void  findByEmailAndStatus_데이터가_없으면_Optional_empty_를_내려준다() {
+        // given
+        // when
+        Optional<UserEntity> byIdAndStatus = userRepository.findByEmailAndStatus("test", UserStatus.PENDING);
+
+        // then
+        Assertions.assertThat(byIdAndStatus.isEmpty()).isTrue();
+    }
+
+
+}
