b4cad8d52bdf10d42019d1770cf017402aad250b
eunkyung <yhr05008@naver.com>
2026-03-15 03:07:39 +0900

    PostControllerTest  테스트 코드 추가

---
+++ src/test/java/com/example/demo/controller/PostCreateControllerTest.java
@@ -0,0 +1,72 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.UserStatus;
+import com.example.demo.model.dto.PostCreateDto;
+import com.example.demo.repository.UserEntity;
+import com.example.demo.repository.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+@Transactional
+class PostCreateControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private long activeUserId;
+
+    @BeforeEach
+    void setUp() {
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setNickname("test");
+        userEntity.setEmail("test@test.com");
+        userEntity.setAddress("Seoul");
+        userEntity.setStatus(UserStatus.ACTIVE);
+        userEntity.setCertificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+        activeUserId = userRepository.save(userEntity).getId();
+
+    }
+
+    @Test
+    void PostCreateDto를_사용하여_게시물을_생성할_수_있다() throws Exception {
+        // given
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .content("new content")
+                .writerId(activeUserId)
+                .build();
+
+
+        // when
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(postCreateDto)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.content").value("new content"))
+                .andExpect(jsonPath("$.writer.id").value(activeUserId));
+    }
+
+
+}
